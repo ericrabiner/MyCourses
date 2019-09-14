c401a341073f256f0d0659264d4ac1dae2f53b16
Eric Rabiner <erabiner@myseneca.ca>
2019-09-14 10:39:03 -0700

    Initial Commit

---
+++ MyCourses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCourses
+//
+//  Created by eric on 2019-09-14.
+//  Copyright © 2019 Eric Rabiner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
